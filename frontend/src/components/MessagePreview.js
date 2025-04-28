0cb327b6f197708b76785e283d9d0efcc6e99b90
Leonardo Medeiros <lmedeiiroos@gmail.com>
2025-04-28 15:25:25 -0300

    Linktree

--- frontend/src/components/MessagePreview.js
+++ frontend/src/components/MessagePreview.js
@@ -236,7 +236,7 @@ const MessagePreview = ({
     // Adicionar link do produto
     message += `\n🛒 ${productUrl}`;
     
-    message += `\n\n☑️ Link do grupo: https://linktr.ee/gdfit`;
+    message += `\n\n☑️ Link do grupo: https://linktr.ee/techdealsbr`;
     
     return message;
   };
